5e82ed65145ebe7fe5445c188f8eca63fcbb72e0
Aljohn_Moorhead <282528950+Aljohn25@users.noreply.github.com>
2026-08-09 19:58:55 +0800

    working buttons(needs refinement) and sketchpad resized

--- javascript.js
+++ javascript.js
@@ -1,18 +1,79 @@
-let vert = 16;
-let horz = 16;
+let normalSize = 16;
 
 const sketchPad = document.querySelector("#sketchPad");
 
-function createGrid() {
-    for (let i = 0; i < (horz * vert); i++) {
-        const gridCell = document.createElement("div");
+function createGrid(size) {
+  const sketchPad = document.querySelector("#sketchPad");
+  
+  sketchPad.innerHTML = ""; 
 
-        gridCell.style.flex = `0 0 ${100 / horz}%`;
-        gridCell.style.height = `${100 / vert}%`;
+  const totalCells = size * size;
+  for (let i = 0; i < totalCells; i++) {
+    const cell = document.createElement("div");
+   cell.classList.add("cell");
 
-        gridCell.classList.add("cell");
-        sketchPad.appendChild(gridCell);
-    } 
+    cell.style.width = `calc(100% / ${size})`;
+    cell.style.height = `calc(100% / ${size})`;
 
+    sketchPad.appendChild(cell);
+  }
 }
-createGrid();
+createGrid(normalSize);
+
+const resize = document.querySelector(".resize");
+const resizeModal = document.querySelector("#resizeModal"); 
+const resizeInput = document.querySelector(".resizeInput");
+const enter = document.querySelector(".enter");
+
+resize.onclick = function() {
+    resizeModal.showModal();
+    resizeInput.focus();
+};
+
+const closeResize = document.querySelector(".closeResize");
+closeResize.onclick = function() {
+  resizeModal.close();
+  resizeInput.value = "";
+  error.textContent = "";
+};
+
+const error = document.querySelector(".error");
+
+enter.onclick = function() {
+
+    const newSize = Number(resizeInput.value);
+    if (newSize > 0 && newSize <= 100) {
+    createGrid(newSize); 
+    resizeModal.close();  
+    resizeInput.value = ""; 
+    error.textContent = "";
+  } else {
+    error.textContent = "Invalid size! Enter 1-100."
+    error.style.color = "red";
+  }
+};
+
+const reset = document.querySelector(".reset");
+
+reset.onclick = function() {
+createGrid(normalSize);
+}
+
+const color = document.querySelector(".color");
+const colorPicker = document.querySelector("#colorPicker");
+
+let selectedColor = colorPicker.value; // Holds the current selected color
+
+// 1. When the user clicks your button, trigger the native color picker window
+color.onclick = function () {
+  colorPicker.click();
+};
+
+// 2. When the user selects a new color, store its value
+colorPicker.addEventListener("input", (e) => {
+  selectedColor = e.target.value;
+  
+  // Optional: Update your button's border or background to show the selected color
+  colorBtn.style.borderColor = selectedColor;
+});
+
